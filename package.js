85c4041094677099f173eacd1677fd58610531a4
Erickson Delgado <ericksond@gmail.com>
2016-10-02 05:57:48 -0400

    Updated moment.js to 2.15.1 and clndr.js to latest. Used minified versions.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ericksond:clndr',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Wrapper for clndr.js',
   git: 'https://github.com/ericksond/meteor-clndr',
   documentation: 'README.md'
@@ -8,6 +8,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.4.2');
-  api.addFiles(['moment.js', 'clndr.js'], 'client');
+  api.addFiles(['moment.min.js', 'clndr.min.js'], 'client');
 });
 
